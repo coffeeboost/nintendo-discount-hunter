851de55ea7b49ad8bd2b0705b4b9bce9bb7294c7
coffeeboost <gtcanada0730@gmail.com>
2021-06-04 19:16:07 -0400

    small updates

--- my_script.sh
+++ my_script.sh
@@ -2,6 +2,7 @@
 if [[ -f process_data.ipynb ]]
 then
  jupyter nbconvert --execute process_data.ipynb
+ rm process_data.html
 else
  echo "source file is not found"
 fi
---
+++ open.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+open "https://www.nintendo.com/games/game-guide/#filter/:q=&dFR[generalFilters][0]=Deals&dFR[platform][0]=Nintendo%20Switch"
+open -e data.txt
